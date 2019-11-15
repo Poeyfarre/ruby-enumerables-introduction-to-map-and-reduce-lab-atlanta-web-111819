0a989e24b1cf03e2e4cfa2460080e128053a25e6
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 13:40:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,17 @@ end
 return total
 end
 
+reduce_to_all_true(source_array)
+index = 0 
 
+while index < source_array.length do
+  if source_array[index] == false
+    return false
+end
+index += 1 
+end
+return true
+end
 
 
 
